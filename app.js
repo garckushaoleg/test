803f9fac3323fb1adff1fc2ad86cde83997ef8a4
Oleg Garckusha <garckushaoleg@gmail.com>
2019-03-04 23:28:36 +0200

    Домашнее задание 3

---
+++ app.js
@@ -0,0 +1,49 @@
+let result = 0;
+
+//Первое модальное окно
+let questionOne = prompt('Сколько будет 2+2');
+
+if (questionOne === '4') {
+    result+=10;
+} else {
+    result+=0;
+}
+
+//Подсказки
+if (questionOne == '') {
+    alert('Вы ничего не ввели!');
+}
+
+if (Number(questionOne)-Number(questionOne)!==0) {
+    alert('Нужно было ввести число!');
+}
+
+//Второе модальное окно
+let questionTwo = confirm('Солнце встает на востоке?');
+
+if (questionTwo === true) {
+    result+=10;
+} else {
+    result+=0;
+}
+
+//Третье модальное окно
+let questionThree = prompt('Сколько будет 5 << 2');
+
+if (questionThree === '20') {
+    result+=10;
+} else {
+    result+=0;
+}
+
+//Подсказки
+if (questionThree == '') {
+    alert('Вы ничего не ввели!');
+}
+
+if (Number(questionThree)-Number(questionThree)!==0) {
+    console.log (questionThree);
+    alert('Нужно было ввести число!');
+}
+
+alert('Вы набрали ' + result + ' баллов')
